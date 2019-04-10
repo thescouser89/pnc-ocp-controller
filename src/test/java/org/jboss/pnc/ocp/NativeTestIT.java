4a53a75be4836eee4ee8a5df54e8a08bc861c39e
Dustin Kut Moy Cheung <dcheung@redhat.com>
2019-04-10 15:32:26 -0400

    Initial commit

---
+++ src/test/java/org/jboss/pnc/ocp/NativeTestIT.java
@@ -0,0 +1,9 @@
+package org.jboss.pnc.ocp;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeTestIT extends TestTest {
+
+    // Execute the same tests but in native mode.
+}
